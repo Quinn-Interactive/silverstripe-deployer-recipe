bf848698365a23c5e0b9e5dde05ad19115158900
Fred Condo <fcondo@quinn.com>
2023-02-17 12:30:43 -0800

    Nginx reload only needed once per server

--- recipe/silverstripe.php
+++ recipe/silverstripe.php
@@ -219,7 +219,7 @@ task('composer:vendor-expose', function () {
 // NGINX
 task('nginx:reload', function () {
     run(sprintf('[ -x %s ] && sudo service nginx reload || exit 0', get('nginx_path', '/usr/local/sbin/nginx')));
-})->desc('Reload the nginx service');
+})->desc('Reload the nginx service')->oncePerNode();
 
 // dev/build OVERRIDES to add 'sudo -Eu www'
 task('silverstripe:build', function () {
