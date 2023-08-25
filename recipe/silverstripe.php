0a02efee21a74404bf90071f5b6f232b5dc41abf
Fred Condo <fcondo@quinn.com>
2023-08-25 14:40:14 -0700

    Ensure existence of local assets directory
    It's often absent if you have just checked out the project.

--- recipe/silverstripe.php
+++ recipe/silverstripe.php
@@ -107,6 +107,7 @@ task('upload', function () {
     // tar up the local assets
     info('Building local assets archive');
     $local_tar_file = tempnam(sys_get_temp_dir(), 'assetstar');
+    runLocally('[ -d public/assets ] || mkdir public/assets');
     runLocally(sprintf("cd public && tar -cf %s %s", $local_tar_file, 'assets'));
 
     // Upload the assets tar file
@@ -183,6 +184,7 @@ task('download', function () {
     // tar up the local assets
     info('Building local assets archive');
     $local_tar_file = tempnam(sys_get_temp_dir(), 'assetstar');
+    runLocally('[ -d public/assets ] || mkdir public/assets');
     runLocally(sprintf("cd public && tar -cf %s %s", $local_tar_file, 'assets'));
 
     // Download the remote assets tar file
